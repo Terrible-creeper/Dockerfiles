7b5482aa83f5940f13af85805551fdea4d71db88
Terrible-creeper <32015407+Terrible-creeper@users.noreply.github.com>
2021-12-17 22:22:10 +0800

    Update entrypoint.sh

--- Nginx/entrypoint.sh
+++ Nginx/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-TZ=${TZ:-UTC}
-export TZ
+cd /home/container
 
-INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
-export INTERNAL_IP
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-cd /home/container || exit 1
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-
-exec env ${PARSED}
+eval ${MODIFIED_STARTUP}
