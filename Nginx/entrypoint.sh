e10754362dd7daf95da08c9518ae5dacb5cffb4c
Terrible-creeper <32015407+Terrible-creeper@users.noreply.github.com>
2021-12-20 09:42:16 +0800

    Update entrypoint.sh

--- Nginx/entrypoint.sh
+++ Nginx/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 cd /home/container
 
+# Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Run the Server
 eval ${MODIFIED_STARTUP}
